c11658859c6431c17b697fefe79d5e83a1e396f4
TomasBattistella <tomas.battistella0@gmail.com>
2025-04-15 16:45:28 -0300

    [Parte 4.1] Crea un form mas las vistas vote y results

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from .models import Question
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.db.models import F
+from django.urls import reverse
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
 
 def detail(request, question_id):
     return HttpResponse("Youre looking at question %s" %question_id)
-
-def results(request, question_id):
-    response = "Youre looking at the result of question %s" 
-    return HttpResponse(response %question_id)
-
-def vote(request, question_id):
-    return HttpResponse("Youre voting on question %s" %question_id)
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -24,3 +19,25 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didnt select a choice"
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
+    
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id) 
+    return render(request, "polls/results.html", {"question": question})
